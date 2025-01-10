02d85e3fe4af9d97b91713e9507628606f2a5492
santu24 <santhosh.vc24@gmail.com>
2025-01-10 17:40:33 +0530

    Data validation completed

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from breast_cancer.components.data_ingestion import DataIngestion
+from breast_cancer.components.data_validation import DataValidation
 from breast_cancer.exception.exception import BreastCancerException
 from breast_cancer.logging.logger import logging
-from breast_cancer.entity.config_entity import DataIngestionConfig
+from breast_cancer.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from breast_cancer.entity.config_entity import TrainingPipelineConfig
 import sys
 
@@ -12,6 +13,12 @@ if __name__=='__main__':
         data_ingestion=DataIngestion(dataingestionconfig)
         logging.info("Initiate the data ingestion")
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
+        logging.info("Data initiation completed")
         print(dataingestionartifact)
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        logging.info('Initiate data validation')
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info('Data validation done')
     except Exception as e:
         raise BreastCancerException(e,sys)
